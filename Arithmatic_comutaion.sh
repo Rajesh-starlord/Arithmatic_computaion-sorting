2c833152aabccc6aad5ee88a0c32cab8d4631963
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-20 15:12:55 +0530

    added uc-4

--- Arithmatic_comutaion.sh
+++ Arithmatic_comutaion.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -x
-#computaion-2
+#computaion-3
 
 read -p "enter no1:-" a
 read -p "enter no2:-" b
@@ -7,8 +7,9 @@ read -p "enter no3:-" c
 
 compute () {
 	result1=`awk "BEGIN {print $a + $b * $c }"`
-	result2==`awk "BEGIN {print $a * $b + $c }"`
-	echo "computation-2 is done result=$result2"
+	result2=`awk "BEGIN {print $a * $b + $c }"`
+	result3=`awk "BEGIN {print $c + $a / $b }"`
+	echo "computation-3 is done result=$result3"
 }
 
 if [[  $a =~ ^[0-9]+$ ]] && [[ $b =~ ^[0-9]+$ ]] && [[ $c =~ ^[0-9]+$ ]];
