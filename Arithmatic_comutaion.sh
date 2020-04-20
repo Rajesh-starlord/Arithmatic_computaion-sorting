477e32428dfb5db61a73182c71a7a10160e2aa4c
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-20 15:04:06 +0530

    uc-2 added

--- Arithmatic_comutaion.sh
+++ Arithmatic_comutaion.sh
@@ -1,10 +1,18 @@
 #! /bin/bash -x
+#computaion-1
+
 read -p "enter no1:-" a
 read -p "enter no2:-" b
 read -p "enter no3:-" c
+
+compute () {
+	result1=`awk "BEGIN {print $a + $b * $c }"`
+	echo $result1
+}
+
 if [[  $a =~ ^[0-9]+$ ]] && [[ $b =~ ^[0-9]+$ ]] && [[ $c =~ ^[0-9]+$ ]];
 then
-    	echo "$a $b $c"
+    	compute
 else
 	echo "Invalid Input:enter some integers"
 fi
