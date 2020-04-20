4ee0bf508237397ff710d6a3e1ca222298f71041
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-20 15:27:02 +0530

    added uc-6

--- Arithmatic_comutaion.sh
+++ Arithmatic_comutaion.sh
@@ -10,8 +10,17 @@ compute () {
 	result2=`awk "BEGIN {print $a * $b + $c }"`
 	result3=`awk "BEGIN {print $c + $a / $b }"`
 	result4=`awk "BEGIN {print $a % $b + c }"`
-	echo "computation-4 is done result=$result4"
+	result_dict=(result1:$result1 result2:$result2 result3:$result3 result4:$result4)
+	echo -e "results stored in a dictionary\n"
+	print_result ${result_dict[@]}
 }
+print_result () {
+	for data in $@
+	do
+		echo -e "$data\n"
+	done
+}
+
 
 if [[  $a =~ ^[0-9]+$ ]] && [[ $b =~ ^[0-9]+$ ]] && [[ $c =~ ^[0-9]+$ ]];
 then
